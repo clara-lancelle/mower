d29010cec35ba5d04bde4aee54db51cc9b97509a
Lancelle Clara <lancelle.clara@hotmail.com>
2023-04-18 17:35:24 +0200

    Add comments && delete file for python2 && change quotes

--- Mower.py
+++ Mower.py
@@ -1,14 +1,19 @@
-# -*- coding: UTF-8 -*-
 class Mower:
-    actual_pos_y = 0
-    actual_pos_x = 0
-    y_boundary = 0
-    x_boundary = 0
-    dir = ''
-    cardinalities = ["N", "E", "S", "W"]
-    error = ''
+    #actual position of the mower on x
+    actual_pos_y: int = 0
+    #actual position of the mower on x
+    actual_pos_x: int = 0 
+    #x limit
+    y_boundary: int = 0
+    #y limit
+    x_boundary: int = 0
+    #direction
+    dir: str = ''
+    cardinalities = ['N', 'E', 'S', 'W']
+    error: str = ''
 
-    def __init__(self, garden_boundaries, start):
+    #at the call of the mower, give garden boundaries and start position of the mower
+    def __init__(self, garden_boundaries: int, start: str):
         bndr = str(garden_boundaries)
         self.y_boundary = int(bndr[1])
         self.x_boundary = int(bndr[0])
@@ -16,25 +21,28 @@ class Mower:
         self.actual_pos_x = int(start[0])
         self.dir = start[3]
         if((self.actual_pos_x > self.x_boundary) or (self.actual_pos_y > self.y_boundary)):
-            self.error = "La position de la tondeuse est en dehors des limites."
+            self.error = 'La position de la tondeuse est en dehors des limites.'
             print(self.error)
        
+    # move the mower in the requested direction but only if it don't exceed the limit   
     def move_to_dir(self):
-        if(self.dir == "N" and self.actual_pos_y != self.y_boundary):
+        if(self.dir == 'N' and self.actual_pos_y != self.y_boundary):
             self.actual_pos_y += 1
-        elif(self.dir == "W" and self.actual_pos_x != 0):
+        elif(self.dir == 'W' and self.actual_pos_x != 0):
             self.actual_pos_x -= 1
-        elif(self.dir == "S" and self.actual_pos_y != 0):
+        elif(self.dir == 'S' and self.actual_pos_y != 0):
             self.actual_pos_y -= 1
-        elif(self.dir == "E" and self.actual_pos_x != self.x_boundary):
+        elif(self.dir == 'E' and self.actual_pos_x != self.x_boundary):
             self.actual_pos_x += 1
         else:
-            self.error = "Une erreur s'est produite."
-           
+            self.error = 'Une erreur s\'est produite.'
+
+    #change the direction of the mower, for left we have to browse the array of cardinalities
+    # for right to left and vice verso for the right instruction
     def change_dir(self, letter):
         if (self.dir in self.cardinalities):
             dir_in_card = self.cardinalities.index(self.dir)
-            if(letter == "G"):
+            if(letter == 'G'):
                 if(dir_in_card != 0):
                     dir_in_card -= 1
                     self.dir = self.cardinalities[dir_in_card]
@@ -47,21 +55,23 @@ class Mower:
                 else:
                     self.dir = self.cardinalities[0]
         else:
-            self.error = "La direction renseignée est incorrecte."
+            self.error = 'La direction renseignée est incorrecte.'
 
-    def mow(self, ins):
+    # reads the given instructions and call functions to execute them if they're valid,
+    # else, a message is displayed
+    def mow(self, ins: str):
         for letter in ins:
-            if(self.error == ""):
-                if(letter == "A"):
+            if(self.error == ''):
+                if(letter == 'A'):
                     self.move_to_dir() 
-                elif(letter == "G" or letter == "D"):
+                elif(letter == 'G' or letter == 'D'):
                     self.change_dir(letter)
                 else:
-                    self.error = "Commande incorrecte."
+                    self.error = 'Commande incorrecte.'
                     print(self.error)
                     break
             else:
-                self.error = "Impossible d'exécuter la commande."
+                self.error = 'Impossible d\'exécuter la commande.'
                 print(self.error)
                 break
-        print(str(self.actual_pos_x) + str(self.actual_pos_y) + self.dir)
+        print(f'{self.actual_pos_x}{ self.actual_pos_y}{self.dir}')
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-#file Mower.py use python version 2.7.18
-#from Mower import Mower
-#file Mower_typed.py use python version 3.10.6
-from Mower_typed import Mower
+from Mower import Mower
 
 garden_boundaries = 55
 mower_one = Mower(55, '44 S')
